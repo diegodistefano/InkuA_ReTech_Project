c5eb269136db88845d1cdca9c17adc249a0f6caf
diegodistefano <diego.distefano.1989@gmail.com>
2023-10-27 21:12:15 -0300

    Creacion de nuevas paginas, unificacion de Header, textos con Lorem

---
+++ Documentación/ReTech - Sitio Web/js/navbar.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+
+header.innerHTML = `
+  <div class="header">               
+  
+  <nav class="menu">
+  
+    <!-- MENU -->
+    <a href="./index.html">Inicio</a>
+    <a href="./rating.html">Eventos</a>
+    <a href="./services.html">Tutoriales</a>
+    <a href="./services.html">Ubicaciones</a>
+    <a href="./hire.html">Donaciones</a>
+    <a href="./about.html">Nosotros</a>
+    <a href="./contact.html">Contacto</a>
+  
+  </nav>
+  
+  <!-- NOMBRE Y LOGO -->
+  <img src="../../images-Sprint1/favicon.png" alt="logo1">
+  </div>
+`;
